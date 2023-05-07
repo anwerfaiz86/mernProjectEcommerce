3ab88226be5b3032acb95d0a7b010c09e6cd42de
Md Faiz Anwer <faizanwerfaiz120@gmail.com>
2023-05-07 12:42:51 +0530

    Vercel

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const Razorpay = require("razorpay");
-const dotenv = require("dotenv");
+
 
 const { connectDatabase } = require("./config/database");
 
@@ -12,8 +12,9 @@ process.on("uncaughtException", (err) => {
 });
 
 // Config
-
-dotenv.config({ path: "backend/config/config.env" });
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "backend/config/config.env" });
+}
 
 // Connecting to database
 connectDatabase();
